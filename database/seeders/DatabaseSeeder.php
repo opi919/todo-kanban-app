11ba5ac63af9908b26d0cb60100f2cd43194a7b2
opi919 <khandokermomen919@gmail.com>
2025-07-10 10:24:22 +0600

    all users added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use App\Models\User;
 use App\UserRole;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -17,23 +18,59 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@ru.ac.bd',
-            'password' => bcrypt('admin123'),
+            'name' => 'Dr. Md. Saiful Islam',
+            'email' => 'msiscse@ru.ac.bd',
+            'password' => bcrypt('124562'),
             'role' => UserRole::Admin,
         ]);
 
         User::factory()->create([
-            'name' => 'opi',
-            'email' => 'opi@ru.ac.bd',
-            'password' => bcrypt('opi123'),
+            'name' => 'Md Abdul Hakim',
+            'email' => 'hakim@ru.ac.bd',
+            'password' => bcrypt('569603'),
             'role' => UserRole::User,
         ]);
 
         User::factory()->create([
-            'name' => 'sami',
-            'email' => 'sami@ru.ac.bd',
-            'password' => bcrypt('sami123'),
+            'name' => 'Md. Abdullah Farook',
+            'email' => 'farook@ru.ac.bd',
+            'password' => bcrypt('761456'),
+            'role' => UserRole::User,
+        ]);
+        User::factory()->create([
+            'name' => 'Rahel Mahfooz Sarker',
+            'email' => 'rahel@ru.ac.bd',
+            'password' => bcrypt('472632'),
+            'role' => UserRole::User,
+        ]);
+        User::factory()->create([
+            'name' => 'Md. Atiqur Rahman',
+            'email' => 'atiqur@ru.ac.bd',
+            'password' => bcrypt('183277'),
+            'role' => UserRole::User,
+        ]);
+        User::factory()->create([
+            'name' => 'Md. Sohag Hosen',
+            'email' => 'sohag@tu.ac.bd',
+            'password' => bcrypt('499080'),
+            'role' => UserRole::User,
+        ]);
+        User::factory()->create([
+            'name' => 'Mst. Tazmultani Sultana',
+            'email' => 'mita@ru.ac.bd',
+            'password' => bcrypt('830586'),
+            'role' => UserRole::User,
+        ]);
+        User::factory()->create([
+            'name' => 'Momen Khandoker Ope',
+            'email' => 'khandokermomen919@ru.ac.bd',
+            'password' => bcrypt('381699'),
+            'role' => UserRole::User,
+        ]);
+        User::factory()->create([
+            'name' => 'Faizul Islam Faruq',
+            'email' => 'ict_center@ru.ac.bd',
+            'password' => bcrypt('557903'),
             'role' => UserRole::User,
         ]);
     }
