5fd341568c00e5298afc2e163301c87a682ac807
opi919 <khandokermomen919@gmail.com>
2025-07-10 10:30:29 +0600

    all users added

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,18 @@
 
 use App\Models\Task;
 use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Livewire::setUpdateRoute(function ($handle) {
+    return Route::post('/todo/livewire/update', $handle);
+});
+
+Livewire::setScriptRoute(function ($handle) {
+    return Route::get('/todo/vendor/livewire/livewire.js', $handle);
 });
 
 Route::get('/htplnsrekgtslotsvypa', function () {
